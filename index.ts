a137f0eb285fa6794460a7673b20495a062e0c97
TUMIT_HASAN <tumithasan1@gmail.com>
2025-07-20 05:46:47 +0600

    create basic server setup

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
